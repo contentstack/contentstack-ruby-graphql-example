d4755d651250697df691020d6389eb600feb2f2c
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-08-17 19:13:58 +0530

    Update

--- contentstack-dotnet-graphql-example/Models/Asset.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace contentstack_dotnet_graphql_example.Models
-{
-    public class Asset
-    {
-        public string url { get; set; }
-        public string filename { get; set; }
-    }
-}
