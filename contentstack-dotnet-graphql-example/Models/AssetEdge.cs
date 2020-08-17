fc82fda281092af4105f5946861c91b2a1ffb30c
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-08-17 19:13:58 +0530

    Update

--- contentstack-dotnet-graphql-example/Models/AssetEdge.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace contentstack_dotnet_graphql_example.Models
-{
-    public class AssetEdge
-    {
-        public Asset node { get; set; }
-    }
-}
--- contentstack-dotnet-graphql-example/Models/ProductResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace contentstack_dotnet_graphql_example.Models
-{
-    public class ProductResponse
-    {
-        public ProductCollection all_product { get; set; }
-    }
-}
