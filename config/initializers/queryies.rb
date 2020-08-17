2c79050b7ceaa130beaefc300e46c1fccef8a8a6
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-08-17 19:17:37 +0530

    Ruby GraphQL Sample app

---
+++ config/initializers/queryies.rb
@@ -0,0 +1,21 @@
+
+Query = ContentstackRubyGraphqlExample::Client.parse <<-'GRAPHQL'
+    query {
+        all_product(locale:"en-us",limit:10) {
+            total
+            items {
+                title
+                description
+                price
+                featured_imageConnection (limit: 10){
+                    edges{
+                        node {
+                            url
+                            filename
+                        }
+                    }
+                }   
+            }
+        }
+    }
+GRAPHQL
